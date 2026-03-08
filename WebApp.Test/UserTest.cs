3d141f615c3b5eaad768d1299e7023651bc5325e
Noureddin94 <186416386+Noureddin94@users.noreply.github.com>
2026-03-08 22:41:48 +0100

    Test project added

---
+++ WebApp.Test/UserTest.cs
@@ -0,0 +1,23 @@
+﻿using WebApp.Infrastructure.Domain.Entities;
+
+namespace WebApp.Test;
+
+public class UserTest
+{
+    [Fact]
+    public void User_ShouldStoreNameCorrectly()
+    {
+        // Arrange
+        var user = new User()
+        {
+           Name = "Alice",
+           Email = "alice@example.com"
+        };
+
+        // Act
+        var result = user.Name;
+
+        // Assert
+        Assert.Equal("Alice", result);
+    }
+}
